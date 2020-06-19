fd07b0b658a28f31bd82a862a458152d2e5c5d9a
gdxn96 <georgedixon24@gmail.com>
2020-06-20 00:42:44 +0100

    looks to be working

--- main.cpp
+++ main.cpp
@@ -1,76 +1,82 @@
-#include <iostream>
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#endif
+
+#include <SDL2/SDL.h>
+#include <cstdlib>
+#include <thread>
+
+struct context
+{
+    SDL_Renderer *renderer;
+    int iteration;
+};
+
+void loop(void *arg)
+{
+    context *ctx = static_cast<context*>(arg);
+    SDL_Renderer *renderer = ctx->renderer;
+
+    // example: draw a moving rectangle
+
+    // red background
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    // moving blue rectangle
+    SDL_Rect r;
+    r.x = ctx->iteration % 255;
+    r.y = 50;
+    r.w = 50;
+    r.h = 50;
+    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255 );
+    SDL_RenderFillRect(renderer, &r );
+
+    SDL_RenderPresent(renderer);
+
+    ctx->iteration++;
 }
 
-// #include <SDL2/SDL.h>
+int main()
+{
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s", SDL_GetError());
+        return 3;
+    }
+    SDL_Window *window;
+    SDL_Renderer *renderer;
 
-// #ifdef __EMSCRIPTEN__
-// #include <emscripten.h>
-// #endif
+    if (SDL_CreateWindowAndRenderer(255, 255, 0, &window, &renderer)) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create window and renderer: %s", SDL_GetError());
+        return 3;
+    }
 
-// #include <cstdlib>
-// #include <thread>
+    context ctx;
+    ctx.renderer = renderer;
+    ctx.iteration = 0;
+    SDL_Event event;
 
-// struct context
-// {
-//     SDL_Renderer *renderer;
-//     int iteration;
-// };
+    const int simulate_infinite_loop = 1; // call the function repeatedly
+    const int fps = -1; // call the function as fast as the browser wants to render (typically 60fps)
 
-// void mainloop(void *arg)
-// {
-//     context *ctx = static_cast<context*>(arg);
-//     SDL_Renderer *renderer = ctx->renderer;
+    #ifdef __EMSCRIPTEN__
+      emscripten_set_main_loop_arg(loop, &ctx, fps, simulate_infinite_loop);
+    #else
+      while (1) {
+        SDL_PollEvent(&event);
+        if (event.type == SDL_QUIT) {
+            break;
+        }
+        loop(&ctx);
+        // Delay to keep frame rate constant (using SDL).
+        SDL_Delay(20);
+      }
+    #endif
 
-//     // example: draw a moving rectangle
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
-//     // red background
-//     SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-//     SDL_RenderClear(renderer);
-
-//     // moving blue rectangle
-//     SDL_Rect r;
-//     r.x = ctx->iteration % 255;
-//     r.y = 50;
-//     r.w = 50;
-//     r.h = 50;
-//     SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255 );
-//     SDL_RenderFillRect(renderer, &r );
-
-//     SDL_RenderPresent(renderer);
-
-//     ctx->iteration++;
-// }
-
-// int main()
-// {
-//     SDL_Init(SDL_INIT_VIDEO);
-//     SDL_Window *window;
-//     SDL_Renderer *renderer;
-//     SDL_CreateWindowAndRenderer(255, 255, 0, &window, &renderer);
-
-//     context ctx;
-//     ctx.renderer = renderer;
-//     ctx.iteration = 0;
-
-//     const int simulate_infinite_loop = 1; // call the function repeatedly
-//     const int fps = -1; // call the function as fast as the browser wants to render (typically 60fps)
-
-//     #ifdef __EMSCRIPTEN__
-//       emscripten_set_main_loop_arg(mainloop, &ctx, fps, simulate_infinite_loop);
-//     #else
-//       while (1) {
-//         one_iter();
-//         // Delay to keep frame rate constant (using SDL).
-//         SDL_Delay(time_to_next_frame());
-//       }
-//     #endif
-
-//     SDL_DestroyRenderer(renderer);
-//     SDL_DestroyWindow(window);
-//     SDL_Quit();
-
-//     return EXIT_SUCCESS;
-// }
+    return EXIT_SUCCESS;
+}
