ea0fd070645229c025fe81921d8d900ae48c5c14
gdxn96 <georgedixon24@gmail.com>
2020-09-22 22:47:29 +0100

    emscripten still not rendering image

--- compile_emscripten.sh
+++ compile_emscripten.sh
@@ -1,2 +1,1 @@
-emcc main.cpp -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='["png"]' -O3 -o dist/index.js  --preload-file assets
-# cp assets/** dist/assets/
+emcc main.cpp -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s WASM=1 -s SDL2_IMAGE_FORMATS='["png"]' -O3 -o dist/index.js --preload-file ./assets@/assets
